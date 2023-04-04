0f301cc8225b042844dde78a4e1a8618031b1d4c
Jeffrey Long <33353663+jjlong150@users.noreply.github.com>
2023-04-04 15:38:51 -0400

    Update config.js

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -6,7 +6,7 @@ module.exports = {
    */
   title: "Excel to Graphviz",
   description: "Excel to Graphviz Relationship Visualizer",
-  base: "/ExcelToGraphviz/",
+  base: "/ExcelToGraphviz/docs/src/",
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
